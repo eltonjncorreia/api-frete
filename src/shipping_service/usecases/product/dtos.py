bba3babe45edc40522997bd2cc6b992146b0f146
eltonjncorreia <eltonjncorreia@gmail.com>
2023-12-03 18:23:38 -0300

    feat: define dtos Product and Shipping

---
+++ src/shipping_service/usecases/product/dtos.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from src.shipping_service.domain.product.entities import Product, Size
+
+
+@dataclass
+class ProductDTO:
+    height: int
+    width: int
+    weight: int
+
+    def to_product(self) -> Product:
+        dimension = Size(height=self.height, width=self.width)
+        return Product(
+            dimension=dimension,
+            weight=self.weight
+        )
--- src/shipping_service/usecases/shipping/dtos.py
+++ src/shipping_service/usecases/shipping/dtos.py
@@ -1,17 +1,11 @@
 from dataclasses import dataclass
 
-from src.shipping_service.domain.product.entities import Product, Size
+from src.shipping_service.domain.product.entities import Product
 
 
 @dataclass
 class ShippingCalculationDTO:
-    height: int
-    width: int
-    weight: int
-
-    def to_product(self) -> Product:
-        dimension = Size(height=self.height, width=self.width)
-        return Product(
-            dimension=dimension,
-            weight=self.weight
-        )
+    name: str
+    price: float
+    delivery_time: int
+    product: Product
